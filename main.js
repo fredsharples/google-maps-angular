1d5652c0739693840c01d7f89b7ca8e6edcd4824
Fred Sharples <fred@orangedesign.com>
2014-01-27 12:59:41 -0800

    added angular-google-maps library

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
 /**
  * Created by fred on 1/25/14.
  */
-var myApp = angular.module('myApp',[]);
+var myMap = angular.module('myMap',[]);
 
 myApp.factory('Data',function (){
     return {message:"I'm data from a service"};
 })
+
+//Use JQuery to get the external data file
+var vehicleObj = (function () {
+    var json = null;
+    $.ajax({
+        'async': false,
+        'global': false,
+        'url': 'vehicleLocations.json', //local file
+        //'url': 'http://dlpx01.aws.revivermx.com:8080/w1/rest/map/getlocations?pl=6YIT558&fr=2014-01-18+04%3A17&to=2014-01-31+21%3A00',
+        //'url': 'http://dlpx01.aws.revivermx.com:8080/w1/rest/map/getlocations?pl=-1&fr=2014-01-18+04%3A17&to=2014-01-31+21%3A00',
+        //'url': 'http://dlpx01.aws.revivermx.com:8080/w1/rest/map/getlocations?pl=5NQE755&fr=2014-01-1+04%3A17&to=2014-01-31+21%3A00',
+        'dataType': "json",
+        'success': function (data) {
+            json = data;
+        }
+    });
+    return json;
+})();
+
 function FirstCtrl($scope, Data){
     $scope.data = Data;
 }
